e92ac50420a9363636b49d4dcb0a5f050e5b27bf
Francois du Toit <bluegray@users.noreply.github.com>
2015-10-19 08:37:13 +0200

    Change cider connect commands

--- config/shortcuts.el
+++ config/shortcuts.el
@@ -37,9 +37,9 @@
 
 
 ;; cider
-(defun new-cider1 () (interactive) (cider "localhost" 9991))
-(defun new-cider2 () (interactive) (cider "localhost" 9995))
-(defun new-cider3 () (interactive) (cider "localhost" 5656))
+(defun new-cider1 () (interactive) (cider-connect "localhost" 7888))
+(defun new-cider2 () (interactive) (cider-connect "localhost" 9995))
+(defun new-cider3 () (interactive) (cider-connect "localhost" 5656))
 (global-set-key (kbd "<f9>")  'new-cider1)
 (global-set-key (kbd "<f10>") 'new-cider2)
 (global-set-key (kbd "M-<f10>") 'new-cider3)
